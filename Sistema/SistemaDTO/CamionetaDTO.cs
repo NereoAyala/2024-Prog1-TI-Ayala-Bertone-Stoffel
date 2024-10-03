67658f36214a2c5a656ce0ed8febb453c5bca03d
NereoAyala <167773112+NereoAyala@users.noreply.github.com>
2024-10-03 10:05:26 -0300

    De nuevo

--- Sistema/SistemaDTO/CamionetaDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EntitiesDTO
-{
-    public class CamionetaDTO
-    {
-        public string Patente { get; set; }
-        public int TamañoCarga { get; set; }
-        public int DistanciaMax { get; set; }
-    }
-}
