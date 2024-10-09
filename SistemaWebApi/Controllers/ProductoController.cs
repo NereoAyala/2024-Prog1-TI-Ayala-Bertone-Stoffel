e7d984b0ed1abfe7ebcb0e32a2ee9b836657f3de
NereoAyala <167773112+NereoAyala@users.noreply.github.com>
2024-10-09 11:08:07 -0300

    Producto API

---
+++ SistemaWebApi/Controllers/ProductoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDTO;
+using SistemaEntities;
+using SistemaServices;
+
+namespace SistemaWebApi.Controllers
+{
+    [ApiController]
+    [Route("Producto")]
+    public class ProductoController : Controller
+    {
+        ProductoService producto=new ProductoService();
+
+        [HttpPost]
+        public IActionResult AgregarProducto([FromBody]ProductoDTO productoDTO) 
+        {
+            ResultadoEntity resultado = producto.AgregarProducto(productoDTO);
+            if (resultado.Success==false)
+            {
+                return BadRequest(resultado.Errores);
+            }
+            else
+            {
+                return Ok(resultado.Message);
+            }
+        }
+        [HttpPut]
+        public IActionResult ActualizarStock(int id,int stockNuevo) 
+        {
+            ResultadoEntity resultado = producto.ActualizarStockProducto(id,stockNuevo);
+            if (resultado.Success==false)
+            {
+                return BadRequest(resultado.Errores);
+            }
+            else
+            {
+                return Ok(resultado.Message);
+            }
+        }
+    }
+}
